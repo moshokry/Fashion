8ff0fb173016250350bebbe683ff427dc3c9c71a
moshokry <48233864+moshokry@users.noreply.github.com>
2019-04-17 19:47:42 +0200

    Update __manifest__.py

--- Fashion/__manifest__.py
+++ Fashion/__manifest__.py
@@ -1,7 +1,7 @@
 ###################################################################################
 # 
 #    Copyright (C) 2019 Fashion  Flixa Logix
-#
+# 
 #    This program is for flixa Logix software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
 #    published by the Free Software Foundation, either version 3 of the

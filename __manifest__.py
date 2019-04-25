57168391fc9fb9f7d23b5886a008dded400b92b3
moshokry <48233864+moshokry@users.noreply.github.com>
2019-04-25 20:28:22 +0200

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+{'name': 'Fashion Management Application',
+ 'description': 'Fashion Module.',
+ 'author': 'Flix Team',
+ 'depends': ['base'],
+ 'data': [
+    'security/library_security.xml',
+    'security/ir.model.access.csv',
+    'views/library_menu.xml',
+    'views/book_view.xml',
+    'views/book_list_template.xml',
+ ],
+ 'application': True,
+ 'installable': True,
+ }
